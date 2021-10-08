70554ce75f4f1b009be6d604b01a0688185dda3f
Max Nanasy <max.nanasy@gmail.com>
2021-10-07 21:59:40 -0700

    Set name, id, author in mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "Synergism Tree",
+	id: "synergism-tree",
+	author: "Max Nanasy",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
