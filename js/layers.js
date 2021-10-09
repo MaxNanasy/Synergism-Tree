9397e8a5e4615eb8a0479586c23f73afab6e6355
Max Nanasy <max.nanasy@gmail.com>
2021-10-09 15:35:42 -0700

    Remove some OOTB TMT comments and change some formatting

--- js/layers.js
+++ js/layers.js
@@ -1,28 +1,28 @@
 addLayer("d", {
-    name: "diamonds", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "D", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    name: "diamonds",
+    symbol: "D",
+    row: 0,
+    position: 0,
     startData() { return {
         unlocked: true,
         points: new Decimal(0),
     }},
     color: "#4BDC13",
     // TODO Implement actual diamond prestiging
-    requires: Decimal.dInf, // Can be a function that takes requirement increases into account
-    resource: "diamonds", // Name of prestige currency
-    baseResource: "coins", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    requires: Decimal.dInf,
+    resource: "diamonds",
+    baseResource: "coins",
+    baseAmount() {return player.points},
     // TODO Implement actual diamond prestiging
-    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
+    type: "none",
+    exponent: 0.5,
+    gainMult() {
         mult = new Decimal(1)
         return mult
     },
-    gainExp() { // Calculate the exponent on main currency from bonuses
+    gainExp() {
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "d", description: "D: Reset for diamonds", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
@@ -91,7 +91,7 @@ function coinBuildingBuyable({
         },
         effect(x) { return x.mul(baseEffect) },
         title,
-        display() { // Everything else displayed in the buyable button after the title
+        display() {
             const coinGen = buyableEffect(this.layer, this.id)
 
             const totalCoinGen = getBaseCoinBuildingPointGen()
--- js/mod.js
+++ js/mod.js
@@ -7,12 +7,11 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (100), // Used for hard resets and new players
+	initialStartPoints: new Decimal (100),
 	// TODO Match Synergism's offline time mechanics
-	offlineLimit: 72,  // In hours
+	offlineLimit: 72, // In hours
 }
 
-// Set your version in num and name
 let VERSION = {
 	num: "0.0",
 	name: "Literally nothing",
@@ -24,20 +23,16 @@ let changelog = `<h1>Changelog:</h1><br>
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
-// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
-// (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
 
 function getStartPoints(){
 	return new Decimal(modInfo.initialStartPoints)
 }
 
-// Determines if it should show points/sec
 function canGenPoints(){
 	return true
 }
 
-// Calculate points/sec!
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
@@ -57,17 +52,14 @@ function getBaseCoinBuildingPointGen() {
 	return gain
 }
 
-// You can add non-layer related variables that should to into "player" and be saved here, along with default values
 function addedPlayerData() { return {
 	// The Modding Tree doesn't seem to track best player points OOTB
 	best: getStartPoints()
 }}
 
-// Display extra things at the top of the page
 var displayThings = [
 ]
 
-// Determines when the game "ends"
 function isEndgame() {
 	// TODO Implement Synergism's endgame condition, if any
 	return false
@@ -77,17 +69,13 @@ function isEndgame() {
 
 // Less important things beyond this point!
 
-// Style for the background, can be a function
 var backgroundStyle = {
 
 }
 
-// You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+	return 3600
 }
 
-// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
-// you can cap their current resources with this.
 function fixOldSave(oldVersion){
 }
